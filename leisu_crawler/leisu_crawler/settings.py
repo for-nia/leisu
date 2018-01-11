99af489053f1ff9738601b37fa6b9ffd8257c993
yearfu <yearfu@foxmail.com>
2018-01-11 13:36:22 +0000

    add crawler from mobile

--- leisu_crawler/leisu_crawler/settings.py
+++ leisu_crawler/leisu_crawler/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'leisu_crawler.spiders'
 #USER_AGENT = 'leisu_crawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
---
+++ leisu_crawler/leisu_crawler/spiders/ls2.py
@@ -0,0 +1,68 @@
+# coding=utf8
+import scrapy
+import re
+from leisu_crawler.items.Match import Match
+from leisu_crawler.items.Match import Oupei
+from leisu_crawler.items.Match import Yapei
+from leisu_crawler.items.Match import Biglittle
+from leisu_crawler.items.Match import Sanheyi
+from datetime import datetime
+import ast
+import requests
+from datetime import datetime
+import time
+import json
+NAME='LeisuM'
+class LeisuLiveMatches(scrapy.Spider):
+    name = NAME
+    start_urls = []
+
+    fenxi='https://live.leisu.com/shujufenxi-{}/'
+    shypeilv='https://live.leisu.com/3in1-{}/'
+    oupei='https://live.leisu.com/oupei-{}'
+    yapei='https://live.leisu.com/yapan-{}'
+    daxiao='https://live.leisu.com/daxiaoqiu-{}'
+    statistic_url='https://live.leisu.com/detail-{}'
+
+
+    leisu_img_fmt='https://cdn.leisu.com/teamflag_s/%s'
+    session=requests.Session()
+
+    def start_requests(self):
+        urls=['https://api.leisu.com/app/live/live?app=0&lang=0&platform=2&ver=2.6.1']
+	yield scrapy.Request(urls[0],callback=self.parse)
+    
+    def parse(self,response):
+        #res=session.get('https://api.leisu.com/app/live/live?app=0&lang=0&platform=2&ver=2.6.1')
+	print response.body
+        j=json.loads(response.body)
+        mathes=j['matches']
+        events=j['events']
+        for match in mathes:
+            m = Match()
+            m.match_id=match[0]
+            m.league_id=str(match[1])
+            m.league_name=events[str(m.league_id)][0]
+            m.begin_time=datetime.fromtimestamp(match[4])
+            home=match[5]
+            m.home_id=str(home[0])
+            m.home_name=home[1]
+            m.home_score=home[2]
+            away=match[6]
+            m.away_id=str(away[0])
+            m.away_name=away[1]
+            m.away_score=away[2]
+            m.home_head , m.away_head = self.headers(m.match_id)
+            match_found = Match.objects(match_id=m.match_id)
+            if match_found:
+                match_found.update_one(home_score=m.home_score, away_score=m.away_score,
+                                       upsert=True)
+            else:
+                m.save()
+    
+    
+    def headers(self,match_id):
+        res=self.session.get('https://api.leisu.com/app/live/matchdetail?app=0&k=4b5b53f43cc5674897a133bbd49c5c07&pk=qGE50mjp&platform=2&t=%s&ver=2.6.1&sid=%s'%(int(time.time()),match_id))
+        m=json.loads(res.text)
+        return self.leisu_img_fmt%m['home'][2],self.leisu_img_fmt%m['away'][2]
+
