a713c13d981d52b36dd0a254c2533a7b9d379cc7
year <yearfu@foxmail.com>
2018-01-29 23:16:34 +0800

    add proxy

--- leisu_crawler/leisu_crawler/spiders/wuchajian.py
+++ leisu_crawler/leisu_crawler/spiders/wuchajian.py
@@ -7,7 +7,36 @@ import re
 from datetime import datetime
 from leisu_crawler.tools import ttzb
 from leisu_crawler.tools import qqlive
-
+head={u'老鹰':'http://china.nba.com/media/img/teams/logos/ATL_logo.svg',
+u'凯尔特人':'http://china.nba.com/media/img/teams/logos/BOS_logo.svg',
+u'篮网':'http://china.nba.com/media/img/teams/logos/BKN_logo.svg',
+u'黄蜂':'http://china.nba.com/media/img/teams/logos/CHA_logo.svg',
+u'公牛':'http://china.nba.com/media/img/teams/logos/CHI_logo.svg',
+u'骑士':'http://china.nba.com/media/img/teams/logos/CLE_logo.svg',
+u'活塞':'http://china.nba.com/media/img/teams/logos/DET_logo.svg',
+u'步行者':'http://china.nba.com/media/img/teams/logos/IND_logo.svg',
+u'热火':'http://china.nba.com/media/img/teams/logos/MIA_logo.svg',
+u'雄鹿':'http://china.nba.com/media/img/teams/logos/MIL_logo.svg',
+u'尼克斯':'http://china.nba.com/media/img/teams/logos/NYK_logo.svg',
+u'魔术':'http://china.nba.com/media/img/teams/logos/ORL_logo.svg',
+u'76人':'http://china.nba.com/media/img/teams/logos/PHI_logo.svg',
+u'猛龙':'http://china.nba.com/media/img/teams/logos/TOR_logo.svg',
+u'奇才':'http://china.nba.com/media/img/teams/logos/WAS_logo.svg',
+u'独行侠':'http://china.nba.com/media/img/teams/logos/DAL_logo.svg',
+u'掘金':'http://china.nba.com/media/img/teams/logos/DEN_logo.svg',
+u'勇士':'http://china.nba.com/media/img/teams/logos/GSW_logo.svg',
+u'火箭':'http://china.nba.com/media/img/teams/logos/HOU_logo.svg',
+u'快船':'http://china.nba.com/media/img/teams/logos/LAC_logo.svg',
+u'湖人':'http://china.nba.com/media/img/teams/logos/LAL_logo.svg',
+u'灰熊':'http://china.nba.com/media/img/teams/logos/MEM_logo.svg',
+u'森林狼':'http://china.nba.com/media/img/teams/logos/MIN_logo.svg',
+u'鹈鹕':'http://china.nba.com/media/img/teams/logos/NOP_logo.svg',
+u'雷霆':'http://china.nba.com/media/img/teams/logos/OKC_logo.svg',
+u'太阳':'http://china.nba.com/media/img/teams/logos/PHX_logo.svg',
+u'开拓者':'http://china.nba.com/media/img/teams/logos/POR_logo.svg',
+u'国王':'http://china.nba.com/media/img/teams/logos/SAC_logo.svg',
+u'马刺':'http://china.nba.com/media/img/teams/logos/SAS_logo.svg',
+u'爵士':'http://china.nba.com/media/img/teams/logos/UTA_logo.svg'}
 leagues=[u'西甲',u'荷甲',u'法甲',u'意甲',u'英超']
 class Wuchajian(scrapy.Spider):
     name = "Wuchajian"
@@ -36,15 +65,18 @@ class Wuchajian(scrapy.Spider):
             match.begin_time=datetime.strptime(begin_time,'%Y-%m-%d %H:%M')
             match.m_from='wuchajian'
             match.channels=[]
-            match_found=Match.objects(match_id=match_id)
+            match_found=Match.objects(wcj_id=match_id)
             match.stream=1
             match.status=1
+	    match.wcj_id=match_id
             #print match.away_name + ' vs ' +match.home_name
             if match_found:
                 self.handle_channel(live_link,match_found[0])
                 self.find_ls_match(match_found[0])
             else:
                 if match.league_name == 'NBA':
+		    match.home_head=head[match.home_name.strip()]
+		    match.away_head=head[match.away_name.strip()]
                     match.save()
                     self.handle_channel(live_link,match)
                 elif match.league_name in leagues:
@@ -74,13 +106,13 @@ class Wuchajian(scrapy.Spider):
                 m=re.findall(r'ttzb\d+',link)
                 channel_name=m[0]
             elif 'cctv5MD' in link:
-                channel_name='cctv5MD'
+                channel_name='cctv5dd2'
             elif 'cctv5jia' in link:
                 channel_name='cctv5jia'
             elif 'cctv5SD' in link:
-                channel_name='cctv5SD'
+                channel_name='cctv52JRS'
             elif 'cctv5' in link:
-                channel_name='cctv5'
+                channel_name='cctv5dd2'
 
             if channel_name:
                 if channel_name in match.channels:
@@ -99,5 +131,5 @@ class Wuchajian(scrapy.Spider):
     def add_channel(self,channel_name):
         if 'ttzb' in channel_name:
             ttzb.add_channel(channel_name)
-        elif 'qqlive' in channel_name:
+        else :
             qqlive.add_channel(channel_name)
--- leisu_crawler/leisu_crawler/tools/qqlive.py
+++ leisu_crawler/leisu_crawler/tools/qqlive.py
@@ -7,14 +7,26 @@ from selenium.webdriver.common.by import By
 import urlparse
 import sys
 from leisu_crawler.items.Match import Channel
+import os
+service_args = ['--proxy=127.0.0.1:9050','--proxy-type=socks5',]
+
+def change_ip():
+    os.system("""(echo authenticate '"hi@tor"'; echo signal newnym; echo \
+          quit) | /bin/nc localhost 9051""")
 
 def get_url(num):
+    return get_by_channel_name(u'qqliveHd{}'.format(num))
+def get_by_channel_name(name):
     webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.settings.userAgent']='Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1'
     webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.customHeaders.referer']='http://www.zuqiu.me/tv/qqlive41.html'
-    driver = webdriver.PhantomJS()
-    driver.get('http://w.zhibo.me:8088/qqliveHD{}.php'.format(num))
+    driver = webdriver.PhantomJS(service_args=service_args)
+    change_ip()
+    driver.get('http://w.zhibo.me:8088/{}.php'.format(name))
+    print driver.page_source
     frames=driver.find_elements(By.TAG_NAME,'iframe')
+    print frames
     videos=driver.find_elements(By.TAG_NAME,'video')
+    print videos
     url = videos[0].get_attribute('src') if len(videos)>0 else frames[0].get_attribute('src') if len(frames)>0 else ''
     if len(frames)>0:
         url = get_stream(url)
@@ -31,7 +43,10 @@ def add_channel(channel_name):
     if channel_found:
         return
     channel=Channel()
-    channel.pc_stream=get_url(channel_name[6:])
+    if u'qqlive' in channel_name:
+        channel.pc_stream=get_url(channel_name[6:])
+    else:
+        channel.pc_stream=get_by_channel_name(channel_name)
     channel.m_stream=channel.pc_stream
     channel.channel_name=channel_name
     channel.c_from='qqlive'
@@ -45,7 +60,10 @@ def refresh_all():
         refresh(channel)
 
 def refresh(channel):
-    pc_stream=get_url(channel.channel_name[6:])
+    if u'qqlive' in channel_name:
+        pc_stream=get_url(channel_name[6:])
+    else:
+        pc_stream=get_by_channel_name(channel_name)
     print pc_stream
     m_stream=pc_stream
     channel.update(pc_stream=pc_stream,m_stream=m_stream)
@@ -54,5 +72,6 @@ if __name__=='__main__':
     num=1
     if len(sys.argv)>1:
         num=sys.argv[1]
+	print get_url(num)
     else:
         refresh_all()
