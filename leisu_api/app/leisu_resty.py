913ea1281b6af831ef191c61962a6ebae11b2842
funiannian <funiannian@a8sport.com>
2018-01-30 16:40:38 +0800

    update

--- leisu_api/app/leisu_resty.py
+++ leisu_api/app/leisu_resty.py
@@ -87,7 +87,6 @@ def player():
     if match.flv:
         print match.flv
         return render_template('player.html',match=match)
-        pass
     else:
         channel_name=request.args.get('channel')
         print channel_name
@@ -98,7 +97,7 @@ def player():
         #if not channel or (not channel.pc_stream and not channel.m_stream):
         #    channel=Channel() if not channel else channel
         #    channel.pc_stream = parse_stream('http://api.leisu.com/api/livestream?sid=%s&type=1' % gameId)
-        channel = None if not channels else channels[match.channels.index(channel_name)] if channel_name in match.channels else channels[0]
+        channel = None if not channels else channels[match.channels.index(channel_name) if channel_name in match.channels else 0]
         return render_template('player.html',channels=channels,match=match,channel=channel)
 
 @app.route('/leisu')
